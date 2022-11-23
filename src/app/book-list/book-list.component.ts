841c22031f98a90be3e819d013cd0b817760bf28
Agustin <aemelzi@gmail.com>
2022-11-23 18:53:16 -0300

    primer componente

---
+++ src/app/book-list/book-list.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-book-list',
+  templateUrl: './book-list.component.html',
+  styleUrls: ['./book-list.component.scss']
+})
+export class BookListComponent implements OnInit {
+
+  book = {
+    "titulo" : "Al asalto del Cielo",
+    "autor" : "Louis De Wohl",
+    "editorial" : "Arcaduz",
+    "imagen" : "assets/img/al asalto del cielo.jpg"
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
