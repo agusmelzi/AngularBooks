4c9f9095a334d941454aa3e914bc0a1f0de14bff
Agustin <aemelzi@gmail.com>
2022-11-24 12:39:42 -0300

    agrego routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BookListComponent } from './book-list/book-list.component';
+import { LibraryAboutComponent } from './library-about/library-about.component';
+import { CartComponent } from './cart/cart.component';
+import { LibraryBooksComponent } from './library-books/library-books.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BookListComponent
+    BookListComponent,
+    LibraryAboutComponent,
+    CartComponent,
+    LibraryBooksComponent
   ],
   imports: [
     BrowserModule,
